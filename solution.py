2a6bdafab7d008dc03433488f99e7dd336c682ea
Hardbasser <glebtevyashov96@yandex.ru>
2020-04-20 20:34:12 +0300

    autoway

--- solution.py
+++ solution.py
@@ -1,9 +1,3 @@
-hour1 = int(input())
-min1 = int(input())
-sec1 = int(input())
-hour2 = int(input())
-min2 = int(input())
-sec2 = int(input())
-delta = str(hour2 * 60 * 60 + min2 * 60 + sec2 -
-            hour1 * 60 * 60 - min1 * 60 - sec1)
-print(delta)
+n = int(input())
+m = int(input())
+print((m + n - 1) // n)
