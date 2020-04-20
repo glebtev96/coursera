0495e3d5c2d732856c1133b5964384972d0a9ec9
Hardbasser <glebtevyashov96@yandex.ru>
2020-04-20 18:29:56 +0300

    cost1

---
+++ solution.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+n = int(input())
+cost = a * 100 + b
+print(n * cost // 100, n * cost % 100)
---
+++ test.py
@@ -0,0 +1,66 @@
+'''print('1','2','3',sep=' + ', end=' ')
+print('=', 1 +2 +3)'''
+'''print(11 +6)
+print(11 - 6)
+print(11 * 6)
+print(11**6)
+print(11 // 6)
+print(11 % 6)
+print((7 - 8) % 24)
+speed1 = 108
+speed2 = -5
+time = 12
+dist = time * (speed1 + speed2)
+print(dist)
+phrase = 'Hasta la vista'
+who = '"baby"'
+print(phrase,',',who,'!', sep='')
+ans = 2 + 3
+expr = '2 + 3 = '
+print(expr + str(ans))
+print(ans + 1)
+print('abc' * 3)
+name = input()
+print('Hello,', name)
+print(int('100' * 100)**2)///
+name = input()
+print('Hello,', name + '!')///
+N = int(input())
+print('   _~_    ' * N)
+print('  (o o)   ' * N)
+print(' /  V  \  ' * N)
+print('/(  _  )\ ' * N)
+print('  ^^ ^^   ' * N)///
+N = int(input())
+K = int(input())
+print(K // N)///
+N = int(input())
+K = int(input())
+print(K % N)///
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+cost1 = a * 100 + b
+cost2 = c * 100 + d
+total_cost = cost1 + cost2
+print(total_cost // 100, total_cost % 100)///
+n = int(input())
+print(2 ** n)///
+n = int(input())
+print(n // 10)///
+n = int(input())
+print((n // 10) % 10)///
+n = int(input())
+print(n // 100 + (n % 100) // 10 + n % 10)///
+print('A' * 100)///
+n = int(input())
+print((n // 60) % 24, n % 60)///
+n = int(input())
+k = int(input())
+print(n // 10 ** k)
+a = int(input())
+b = int(input())
+print((a + b - 1) // b)
+
+'''
